fb9afb564dfd3c3d1cd2abf8549069ed7e1ed403
tawaluk <tawaluk@yandex.ru>
2023-11-12 01:55:44 +0300

    первая попытка

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = os.getenv('DEBUG', default='False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*')
 
 AUTH_USER_MODEL = 'users.UserFoodgram'
 
