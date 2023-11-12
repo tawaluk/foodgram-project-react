55046c962282cc39d3b77bc87956ad6930d32006
tawaluk <tawaluk@yandex.ru>
2023-11-12 15:14:55 +0300

    прощай дебаг

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY',
     default='django-insecure-a2o85^^_u*w%6v8jlscory(xx)(mc=68ax)$)kdqs0cdn7uvnd')
 
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
