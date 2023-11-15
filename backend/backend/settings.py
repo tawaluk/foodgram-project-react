9be5129b94ae9aee964629e48a0a1d09563aa161
tawaluk <tawaluk@yandex.ru>
2023-11-15 09:55:05 +0300

    день 4. вернул фронт, исправляю замечания

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY',
     default='django-insecure-a2o85^^_u*w%6v8jlscory(xx)(mc=68ax)$)kdqs0cdn7uvnd')
 
-DEBUG = os.getenv('DEBUG', default='False') == 'True'
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = ['https://workexampletavalyuk.ru']
 
