61557bdbc3ec4d9651a7d7b0cdee7fe84d5f3396
tawaluk <101930007+tawaluk@users.noreply.github.com>
2023-11-16 07:16:29 +0300

    Update paginators.py

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -2,6 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    """Паджинатор для проекта фудграм."""
 
     page_size_query_param = "limit"
