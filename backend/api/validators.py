a3eeea68b02fc064e3e2c0393b4afbc60d9821f6
tawaluk <101930007+tawaluk@users.noreply.github.com>
2023-11-16 07:15:13 +0300

    Update validators.py

--- backend/api/validators.py
+++ backend/api/validators.py
@@ -3,6 +3,5 @@ from rest_framework.response import Response
 
 
 def validation_400(value):
-    """Свой кастыльный валидатор"""
     if not value:
         return Response(status=status.HTTP_400_BAD_REQUEST)
